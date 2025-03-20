238da13547c55ee39160aa9c3e03a9ed6b1aea08
nikosemp <nikosemp@hotmail.com>
2025-03-20 02:25:24 +0200

    Initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+/*   const [submitted, setSubmitted] = useState(''); */
+
+/*   function handleChange(evt) {
+    setSubmitted(false);
+    setEnteredPlayerName(evt.target.value);
+  } */
+
+  function handleClick(){
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}</h2>
+      <p>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
